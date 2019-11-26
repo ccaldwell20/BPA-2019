f7d4e5478d288a119b5faa90eef2bb742bbbf538
Christian Caldwell <ccaldwell20@sciototech.org>
2019-11-26 13:02:36 -0500

    ProductArrayParser Update

--- js/water.js
+++ js/water.js
@@ -1,1 +1,1 @@
- 
+var mainProducts = ["Water Product 1", "This is a test description.", "testImage.png", 150, 150, 1750, 0,"Water Product 2", "This is a test description.2", "testImage.png", 200, 200, 3655, 0]; 
--- js/wind.js
+++ js/wind.js
@@ -1,1 +1,1 @@
- 
+var mainProducts = ["Windmill 1", "This is a test description.", "testImage.png", 150, 150, 1750, 0,"Windmill 2", "This is a test description.2", "testImage.png", 200, 200, 3655, 0]; 
